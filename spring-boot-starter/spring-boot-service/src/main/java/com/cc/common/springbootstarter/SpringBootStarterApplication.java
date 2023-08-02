eba73ad188937b52911bb6cd5bba72cf493313c6
khu5h1 <khushirauniyar8@gmail.com>
2023-08-02 20:54:12 +0530

    first commit

---
+++ spring-boot-starter/spring-boot-service/src/main/java/com/cc/common/springbootstarter/SpringBootStarterApplication.java
@@ -0,0 +1,14 @@
+package com.cc.common.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterApplication.class, args);
+		System.out.println("Credit Card Universe");
+	}
+
+}
